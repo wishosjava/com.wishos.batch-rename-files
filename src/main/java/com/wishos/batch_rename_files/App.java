13b2859df7b30387d92377b848e6f299766b5f1b
黯然风色 <upc_jibin@yeah.net>
2018-01-24 17:54:08 +0800

    初始化

---
+++ src/main/java/com/wishos/batch_rename_files/App.java
@@ -0,0 +1,13 @@
+package com.wishos.batch_rename_files;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
